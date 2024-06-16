acc379635a7fa529913b8bc31bcd32e91c060a9b
iballibull <muchammadiqbalyusuf@gmail.com>
2024-06-16 13:27:07 +0700

    multer error

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
+const path = require("path");
+const fs = require("fs");
 
 const authRoute = require("./routes/Auth.js");
 const registerRoute = require("./routes/Register.js");
