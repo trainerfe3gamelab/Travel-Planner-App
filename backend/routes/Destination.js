cd0d8b15873c36dc0c3545ea0f67647a74dbb79f
Roni Rahmadi <90230014+ronirahmadi@users.noreply.github.com>
2024-06-16 09:34:26 +0700

    Destination Update

--- backend/routes/Destination.js
+++ backend/routes/Destination.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const destinationController = require("../controllers/Users/destinationController");
-const { isLoggedIn } = require("../middleware/authMiddleware");
-
 // Get all tour 
-router.get('/', isLoggedIn, destinationController.getAllTours);
+router.get('/', destinationController.getAllTours);
 
 // Get single tour 
-router.get('/:id', isLoggedIn, destinationController.getSingleTour);
+router.get('/:id', destinationController.getSingleTour);
 
 // Get tour by search
-router.get("/search/getTourBySearch", isLoggedIn, destinationController.getTourBySearch);
-router.get("/search/getTourCount", isLoggedIn, destinationController.getTourCount);
+router.get("/search/getTourBySearch", destinationController.getTourBySearch);
+router.get("/search/getTourCount", destinationController.getTourCount);
 
 module.exports = router;
