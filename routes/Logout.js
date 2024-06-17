ee0b7b97cbbe159fab9b9b64a52c955a3a0756d4
Roni Rahmadi <90230014+ronirahmadi@users.noreply.github.com>
2024-06-17 14:42:47 +0700

    delete some directory

--- routes/Logout.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const logoutController = require("../controllers/Users/logoutController");
-
-router.get("/", logoutController);
-
-module.exports = router;
--- routes/Review.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const reviewController = require("../controllers/Users/reviewController");
-
-const router = express.Router()
-const { isLoggedIn } = require("../middleware/authMiddleware");
-
-router.post('/:tourId', isLoggedIn, reviewController.createReview)
-
-module.exports = router;
