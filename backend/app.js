292fceb3846a4db0c6db4f8a47f2555972869999
trainerBE3 <163844685+trainerBE3@users.noreply.github.com>
2024-06-17 15:41:43 +0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ const reservationRoutes = require("./routes/Reservation.js");
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8597;
 
 const corsOptions = {
   origin: true,
