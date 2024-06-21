d82a55f138fa761faa4e5b6a887dbf34b5f4c2ed
trainerBE3 <163844685+trainerBE3@users.noreply.github.com>
2024-06-21 15:52:44 +0700

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://api-msib-6-travel-planner-01.educalab.id", // ini url API default
+  baseURL: "https://api-msib-6-travel-planner-01.educalab.id/api", // ini url API default
 });
 
 export default api;
